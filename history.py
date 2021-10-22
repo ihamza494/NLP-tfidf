8ee5bf3a1fd0bca77dc3ed9b0c701f0f427d540f
ihamza494 <ihamza494@gmail.com>
2021-10-22 22:14:54 +0500

    nlp tfidf

---
+++ history.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Fri Oct 22 10:02:32 2021)---
+runfile('C:/Users/ihamz/.spyder-py3/temp.py', wdir='C:/Users/ihamz/.spyder-py3')
+
+## ---(Fri Oct 22 10:15:39 2021)---
+conda update anaconda
+runcell(0, 'C:/Users/ihamz/.spyder-py3/temp.py')
+runfile('C:/Users/ihamz/.spyder-py3/temp.py', wdir='C:/Users/ihamz/.spyder-py3')
+!git add temp.py
+!git commit-m "MY COMMIT"
+!git commit -m "MY COMMIT"
+git push origin master
+!git push origin master
+!git init
+!git commit -m "nlp tfidf"
+git branch -m Main
+!git branch -m Main
+!git remote add origin https://github.com/ihamza494/NLP-tfidf.git
+!git push -u origin main
+!git status
+!git add 
+!git add .
